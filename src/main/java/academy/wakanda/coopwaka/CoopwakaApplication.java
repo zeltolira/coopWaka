61adcafce45290263abbbe7841540f3538558e7d
unknown <zelto@hotmail.com>
2024-04-14 08:21:42 -0300

    init

---
+++ src/main/java/academy/wakanda/coopwaka/CoopwakaApplication.java
@@ -0,0 +1,21 @@
+package academy.wakanda.coopwaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class CoopwakaApplication {
+	@GetMapping
+	public String testeHome(){
+		return "teste Wakacop";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CoopwakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/wakanda/coopwaka/CoopwakaApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.coopwaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopwakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
